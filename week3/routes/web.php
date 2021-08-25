35f102511d2b25c57bef98a083760c1599778099
justatea <s160719008@student.ubaya.ac.id>
2021-08-25 16:26:42 +0700

    week 3_sampai seeding

---
+++ week3/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/menu','menu');
+
+Route::get('/menu/{category}/{product}',function($category='pudding',$product='pudding-fla'){
+    return view('productdetail',['category'=>$category,'product'=>$product]);
+});
